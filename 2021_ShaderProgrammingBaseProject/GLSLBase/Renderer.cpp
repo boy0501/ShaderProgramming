8050c5b416cf831ed815a6af9e9648d4278cdb5b
boy0501 <boy0501@naver.com>
2022-04-26 13:16:07 +0900

    3-5.1

--- 2021_ShaderProgrammingBaseProject/GLSLBase/Renderer.cpp
+++ 2021_ShaderProgrammingBaseProject/GLSLBase/Renderer.cpp
@@ -538,8 +538,12 @@ void Renderer::Lecture3_Particle()
 
 	int uniformTime = glGetUniformLocation(shader, "u_Time");
 	glUniform1f(uniformTime, gTime);
-	gTime += 0.0001f;
-	if (gTime > 1.f) gTime = 0.f;
+	int uniformAccel = glGetUniformLocation(shader, "u_Accel");
+	glUniform3f(uniformAccel, std::sin(gTime), 0.0, 0.0);
+
+
+	gTime += 0.01f;
+	//if (gTime > 1.f) gTime = 0.f;
 
 	glDrawArrays(GL_TRIANGLES, 0, m_VBOManyParticleVertexCount);
 

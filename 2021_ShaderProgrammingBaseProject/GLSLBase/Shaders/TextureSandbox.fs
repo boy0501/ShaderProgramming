f44b8c49e283f9dc7c0bb6358053405d1c57d888
boy0501 <boy0501@naver.com>
2022-06-14 08:06:02 +0900

    8 텍스쳐 두개 사용하기
    숙제로 뭘 내주셨는데 시험에 안나온다는 보장은 없다고,,,하시네염

--- 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/TextureSandbox.fs
+++ 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/TextureSandbox.fs
@@ -5,7 +5,9 @@ layout(location=0) out vec4 FragColor;
 in vec2 v_TexCoord;
 
 uniform sampler2D u_TexSampler;
+uniform sampler2D u_TexSampler1;
 
+uniform float u_Time;
 vec4 Flip()
 {
 	vec4 returnValue = vec4(0);
@@ -48,8 +50,33 @@ vec4 P3()
 	return returnValue;
 }
 
+vec4 P4()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.x = fract(v_TexCoord.x * 2) ;
+	if(v_TexCoord.x > 0.5)
+	{
+		returnValue = texture(u_TexSampler1,newTexCoord);
+	}else{
+		returnValue = texture(u_TexSampler,newTexCoord);
+	}
+
+	return returnValue;
+}
+vec4 P5()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	vec2 newTexCoord1 = v_TexCoord;
+	newTexCoord1.x += u_Time;
+	newTexCoord1.x = fract(newTexCoord1.x);
+	returnValue = texture(u_TexSampler,newTexCoord) * texture(u_TexSampler1,newTexCoord1);
+
+	return returnValue;
+}
 
 void main()
 {
-	FragColor = P3();
+	FragColor = P5();
 }

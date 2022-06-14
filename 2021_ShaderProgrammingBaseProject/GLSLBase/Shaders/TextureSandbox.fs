ae953cb0b93f723f53ae84b9d41efc2580596403
boy0501 <boy0501@naver.com>
2022-06-14 16:31:06 +0900

    6번째 실습(텍스쳐 좌표 꼬기)
    주석에 설명 다 달아놨다잉
    꿀팁 : 구간은 fract로, 통일성은 floor로 !
    if문 절~~~~~~~~~~대 쓰지말고 구하기 !

--- 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/TextureSandbox.fs
+++ 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/TextureSandbox.fs
@@ -76,7 +76,63 @@ vec4 P5()
 	return returnValue;
 }
 
+vec4 P6()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+
+	float v = floor(v_TexCoord.x *2);
+	newTexCoord.x = fract(v_TexCoord.x * 2) ;
+	newTexCoord.y = fract(v_TexCoord.y *2)+ v * 0.5;
+
+
+	returnValue = texture(u_TexSampler,newTexCoord);
+
+	return returnValue;
+}
+
+vec4 P7()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	
+	newTexCoord.x = fract(v_TexCoord.x * 3);
+ 	newTexCoord.y = newTexCoord.y/3.0 + (2 - floor(v_TexCoord.x*3.0))/3.0;
+
+	returnValue = texture(u_TexSampler,newTexCoord);
+
+	return returnValue;
+}
+
+vec4 P8()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	//0~3분의1 , 3분의1~3분의2 ,3분의 2 
+	// TIP : 구간은 fract로, 통일성은 floor로 ! 
+	//floor로 0~0 ,1~1, 2~2를 만들고 여기에 fract를 더해준다. 그럼 0~0.9,1~1.9, 2~2.9 인데 여기서 3으로 나누면 원하는구간 get
+	newTexCoord.y = ((2-floor(v_TexCoord.y * 3)) + fract(v_TexCoord.y * 3)) / 3;
+
+	returnValue = texture(u_TexSampler,newTexCoord);
+
+	return returnValue;
+}
+
+vec4 P9()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	//세로로 두번 그려야하니 0~1,0~1 fract조져 
+	//0~0.5 까지는 뒤집어야함 . floor로 0 , 1 구하고 0.5 빼면 위에는 0.5 아래는 -0.5 곱하기 2 하면 1 ~ -1 아래만 -1 곱해서 반전 해
+	float v = (floor(v_TexCoord.y * 2) - 0.5) * 2;
+	newTexCoord.y = fract(v_TexCoord.y * 2)  * v;
+
+	returnValue = texture(u_TexSampler,newTexCoord);
+
+	return returnValue;
+}
+
 void main()
 {
-	FragColor = P5();
+	FragColor = P7();
 }
